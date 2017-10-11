547194b8b6d5d628be71f7fdc0075868afff0764
kyle <kyle@kyle-Virtualbox>
2017-10-11 14:29:04 -0400

    Syseminfo.sh part2

--- systeminfo.sh
+++ systeminfo.sh
@@ -4,14 +4,36 @@
 # 2017 09 18
 
 # Declare variables and assign any default values.
+
 defaultmode="yes"
 
 export domainnamewanted=""
 export domainnameformatted=""
 
+export IPaddresswanted=""
+export IPaddressformatted=""
+
+export OSversionwanted=""
+export OSversionformatted=""
+
+export CPUinfowanted=""
+export CPUinfoformatted=""
+
+export RAMinfowanted=""
+export RAMinfoformatted=""
+
+export diskspacewanted=""
+export diskspaceformatted=""
+
+export printerwanted=""
+export pirnterformatted=""
+
+export installedsoftwarewanted=""
+export installedsoftwareformatted=""
+
 # Define functions for error messages and displaying command line help.
 function displayinfo {
-  echo "Usage:$0 [-h | --help] [-sn] [-on] [-dn]"
+  echo "Usage:$0 [-h | --help] [-sn] [-on] [-dn] [-ip] [-ov] [-c] [-m] [-ds] [-p] [-s] "
 }
 function replyerror {
   echo "$@" >&2
@@ -40,6 +62,41 @@ while [ $# -gt 0 ]; do
       defaultmode="no"
       ;;
 
+    -ip)
+      IPaddresswanted="yes"
+      defaultmode="no"
+      ;;
+
+    -ov)
+      OSversionwanted="yes"
+      defaultmode="no"
+      ;;
+
+    -c)
+      CPUinfowanted="yes"
+      defaultmode="no"
+      ;;
+
+    -m)
+      RAMinfowanted="yes"
+      defaultmode="no"
+      ;;
+
+    -ds)
+      diskspacewanted="yes"
+      defaultmode="no"
+      ;;
+
+    -p)
+      printerwanted="yes"
+      defaultmode="no"
+      ;;
+
+    -s)
+      installedsoftwarewanted="yes"
+      defaultmode="no"
+      ;;
+
     *)
       replyerror "I don't know what '$1' is. Sorry."
       replyerror "$(displayinfo)"
@@ -54,8 +111,11 @@ osinfo="$(grep PRETTY /etc/os-release |sed -e 's/.*=//' -e 's/"//g')"
 
 domainname="$(grep PRETTY /etc/os-release |sed -e 's/.*=//' -e 's/"//g')"
 
+osversion="$(grep PRETTY /etc/os-release |sed -e 's/.*=//' -e 's/"//g')"
+
 systemname="$(hostname)"
 domainname="$(domainname)"
+osversion="$(osversion)"
 
 
 # Create the output using the gathered data and command line options.
@@ -79,6 +139,12 @@ System Name: $systemname
 Domainname: $domainname
 "
 
+OSversionformatted="
+Operating System Version Information:
+----------------------------
+$osversion
+"
+
 # Display the output.
 if [ "$defaultmode" = "yes" -o "$namesinfowanted" = "yes" ]; then
   echo "$nameinfoformatted"
@@ -90,6 +156,10 @@ fi
 
 if [ "$defaultmode" = "yes" -o  "$domainnamewanted" = "yes" ]; then
   echo "$domainnamewanted"
+fi
+
+if [ "$defaultmode" = "yes" -o  "$OSversionwanted" = "yes" ]; then
+  echo "$OSversionformatted"
 fi
 
 # Do any cleanup of temporary files if needed
